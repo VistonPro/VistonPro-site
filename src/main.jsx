f6e1d9490570012cf33bbdbe0a2a5735e8836d6a
Louis Creviston <vistontraining@gmail.com>
2025-06-25 21:39:58 -0800

    Add routes for Shop and Training pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/tools" element={<ToolsPage />} />
-      </Routes>
+  <Route path="/" element={<Homepage />} />
+  <Route path="/tools" element={<ToolsPage />} />
+  <Route path="/shop" element={<ShopPage />} />
+  <Route path="/training" element={<TrainingPage />} />
+</Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
