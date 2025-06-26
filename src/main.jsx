6568a24f214307e715051352fd6d137ace945e6d
VistonPro <vistontraining@gmail.com>
2025-06-25 19:45:11 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Homepage from "./Homepage";
+import ToolsPage from "./ToolsPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Homepage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/tools" element={<ToolsPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
